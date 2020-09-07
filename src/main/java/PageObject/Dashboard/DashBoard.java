063663220c0dfbd2164ac5f0ba4fb7c8292738da
Sviat <syava1204@gmail.com>
2020-09-07 20:22:44 +0300

    +Several Dashboard Tests

--- src/main/java/PageObject/Dashboard/DashBoard.java
+++ src/main/java/PageObject/Dashboard/DashBoard.java
@@ -5,24 +5,113 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+
 public class DashBoard extends BasePage {
     public DashBoard(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy (xpath = "//div[@class='css-bg1rzq-control select__control']")
+    @FindBy(xpath = "//div[@class='css-bg1rzq-control select__control']")
     private WebElement timePeriodTile;
-    @FindBy (xpath = "//div[@id='react-select-2-option-0']")
+    @FindBy(xpath = "//div[@id='react-select-2-option-0']")
     private WebElement timePeriod24Hours;
-    @FindBy (xpath = "//div[@id='react-select-2-option-1']")
+    @FindBy(xpath = "//div[@id='react-select-2-option-1']")
     private WebElement timePeriod7Days;
-    @FindBy (xpath = "//div[@id='react-select-2-option-2']")
+    @FindBy(xpath = "//div[@id='react-select-2-option-2']")
     private WebElement timePeriod30Days;
 
+    @FindBy(xpath = "//div[contains(@class, 'TabNavItem') and text() = 'Overview']")
+    private WebElement overviewTabBttn;
+    @FindBy(xpath = "//div[contains(@class, 'TabNavItem') and text() = 'Exchange Online']")
+    private WebElement exchangeOnlineTabBttn;
+    @FindBy(xpath = "//div[contains(@class, 'TabNavItem') and text() = 'Exchange Online']")
+    private WebElement oneDriveTabBttn;
 
-    public WebElement getTimePeriodTile (){return timePeriodTile;}
-    public String getTimePeriod24Hours(){return timePeriod24Hours.getText();}
-    public String getTimePeriod7Days(){return timePeriod7Days.getText();}
-    public String getTimePeriod30Days(){return timePeriod30Days.getText();}
+    @FindBy(xpath = "//p[contains(text(),'License usage')]")
+    private WebElement licenseUsageOverviewTile;
+    @FindBy(xpath = "//p[contains(text(),'Top spam recipients')]")
+    private WebElement topSpamRecipientsOverviewTile;
+    @FindBy(xpath = "//p[contains(text(),'Top malware recipients')]")
+    private WebElement topMalwareRecipientsOverviewTile;
+    @FindBy(xpath = "//p[contains(text(),'Top phishing recipients')]")
+    private WebElement topPhishingRecipientsOverviewTile;
+    @FindBy(xpath = "//p[contains(text(),'Top suspicious OneDrive accounts')]")
+    private WebElement topSuspiciousOneDriveAccountsOverviewTile;
 
+    @FindBy(xpath = "//p[contains(text(),'Spam traffic')]")
+    private WebElement spamTrafficExchangeOnlineTile;
+    @FindBy(xpath = "//p[contains(text(),'Malware traffic')]")
+    private WebElement malwareTrafficExchangeOnlineTile;
+    @FindBy(xpath = "//p[contains(text(),'Phishing traffic')]")
+    private WebElement phishingTrafficExchangeOnlineTile;
+
+    @FindBy(xpath = "//p[contains(text(),'Malware traffic')]")
+    private WebElement malwareTrafficOnedriveTile;
+
+
+    public WebElement getTimePeriodTile() {
+        return timePeriodTile;
+    }
+
+    public String getTimePeriod24Hours() {
+        return timePeriod24Hours.getText();
+    }
+
+    public String getTimePeriod7Days() {
+        return timePeriod7Days.getText();
+    }
+
+    public String getTimePeriod30Days() {
+        return timePeriod30Days.getText();
+    }
+
+    public DashBoard clickOverviewTabBttn() {
+        overviewTabBttn.click();
+        return this;
+    }
+
+    public DashBoard clickExchangeOnlineTabBttn() {
+        exchangeOnlineTabBttn.click();
+        return this;
+    }
+
+    public DashBoard clickOneDriveTabBttn() {
+        oneDriveTabBttn.click();
+        return this;
+    }
+
+    public WebElement getLicenseUsageOverviewTile() {
+        return licenseUsageOverviewTile;
+    }
+
+    public WebElement getTopSpamRecipientsOverviewTile() {
+        return topSpamRecipientsOverviewTile;
+    }
+
+    public WebElement getTopMalwareRecipientsOverviewTile() {
+        return topMalwareRecipientsOverviewTile;
+    }
+
+    public WebElement getTopPhishingRecipientsOverviewTile() {
+        return topPhishingRecipientsOverviewTile;
+    }
+
+    public WebElement getTopSuspiciousOneDriveAccountsOverviewTile() {
+        return topSuspiciousOneDriveAccountsOverviewTile;
+    }
+
+    public WebElement getSpamTrafficExchangeOnlineTile() {
+        return spamTrafficExchangeOnlineTile;
+    }
+
+    public WebElement getMalwareTrafficExchangeOnlineTile() {
+        return malwareTrafficExchangeOnlineTile;
+    }
+
+    public WebElement getPhishingTrafficExchangeOnlineTile() {
+        return phishingTrafficExchangeOnlineTile;
+    }
+    public WebElement getMalwareTrafficOnedriveTile() {
+        return malwareTrafficOnedriveTile;
+    }
 }
--- src/test/java/Dashboard/DashboardTests.java
+++ src/test/java/Dashboard/DashboardTests.java
@@ -3,8 +3,8 @@ package Dashboard;
 import BrowsersAndListeners.Browser;
 import PageObject.Constants;
 import PageObject.Dashboard.DashBoard;
-import PageObject.TopHeadings.TopHeadings;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -29,14 +29,42 @@ public class DashboardTests extends Browser {
     }
 
     @Test
-    public void checkPresenceOfTimePeriods(){
+    public void checkPresenceOfTimePeriods() {
         SoftAssert soft = new SoftAssert();
         dashboard.getTimePeriodTile().click();
-        soft.assertEquals(dashboard.getTimePeriod24Hours(),"Last 24 hours");
-        soft.assertEquals(dashboard.getTimePeriod7Days(), "Last 7 days");
-        soft.assertEquals(dashboard.getTimePeriod30Days(),"Last 30 days");
+        soft.assertEquals(dashboard.getTimePeriod24Hours(), "Last 24 hours", "Time Period `Last 24 hours` is not present");
+        soft.assertEquals(dashboard.getTimePeriod7Days(), "Last 7 days", "Time Period `Last 7 days` is not present");
+        soft.assertEquals(dashboard.getTimePeriod30Days(), "Last 30 days", "Time Period `Last 7 days` is not present");
         soft.assertAll();
 
+    }
+
+    @Test
+    public void checkPresenceOfOverviewTabTiles() {
+        SoftAssert soft = new SoftAssert();
+        dashboard.clickOverviewTabBttn();
+        soft.assertTrue(dashboard.getLicenseUsageOverviewTile().isDisplayed(), "License Usage Tile is not present");
+        soft.assertTrue(dashboard.getTopSpamRecipientsOverviewTile().isDisplayed(), "Top Spam Recipients Tile is not present");
+        soft.assertTrue(dashboard.getTopMalwareRecipientsOverviewTile().isDisplayed(), "Top Malware Recipients Tile is not present");
+        soft.assertTrue(dashboard.getTopPhishingRecipientsOverviewTile().isDisplayed(), "Top Phishing Recipients Tile is not present");
+        soft.assertTrue(dashboard.getTopSuspiciousOneDriveAccountsOverviewTile().isDisplayed(), "OneDrive Tile is not present");
+        soft.assertAll();
+    }
+
+    @Test
+    public void checkPresenceOfExchangeOnlineTiles() {
+        SoftAssert soft = new SoftAssert();
+        dashboard.clickExchangeOnlineTabBttn();
+        soft.assertTrue(dashboard.getSpamTrafficExchangeOnlineTile().isDisplayed(), "Spam Traffic table is not present");
+        soft.assertTrue(dashboard.getMalwareTrafficExchangeOnlineTile().isDisplayed(), "Malware Traffic table is not present");
+        soft.assertTrue(dashboard.getPhishingTrafficExchangeOnlineTile().isDisplayed(), "Phishing Traffic table is not present");
+        soft.assertAll();
+    }
+
+    @Test
+    public void checkPresenceOfOnedriveTile(){
+        dashboard.clickOneDriveTabBttn();
+        Assert.assertTrue(dashboard.getMalwareTrafficOnedriveTile().isDisplayed(), "Malware traffic table is not present");
     }
 
     @AfterClass
